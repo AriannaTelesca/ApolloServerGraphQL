1680b90ccb3f089c715025c7cbb9585ddd7de34a
Arianna Telesca <arianna.tlsc@gmail.com>
2022-11-10 09:46:39 +0100

    deploy

--- server/dist/server.js
+++ server/dist/server.js
@@ -20,9 +20,9 @@ const mount = async (app) => {
         const httpServer = (0, http_1.createServer)(app);
         app.use('*', (0, cors_1.default)());
         app.use((0, compression_1.default)());
-        app.use(express_1.default.static(path_1.default.join(__dirname, 'client')));
+        app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
         app.get('*', function (req, res) {
-            res.sendFile(path_1.default.join(__dirname, 'client/build', 'index.html'));
+            res.sendFile(path_1.default.join(__dirname, '../client/build', 'index.html'));
         });
         httpServer.listen({ port: process.env.PORT || 4000 }, () => console.log(`\ Graphql is now running on http://4000/graphql`));
     }
